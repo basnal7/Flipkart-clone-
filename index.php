5a8a1e61a6a489b87d679c45bde416b9ced64077
Hema Basnal <hemabasnal9991@gmail.com>
2022-06-08 13:40:57 +0100

    flipkart-clone -working

---
+++ index.php
@@ -0,0 +1,545 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flipkart clone </title>
+
+    <!-- coustom css -->
+
+    <link rel="stylesheet" href="css/header.css">
+
+    <!-- fontawsome 4 cdn link  -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- bootstrap 4 css -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- bootstrap 4 js -->
+    
+
+
+<!-- jquery cdn link   -->
+
+<script src="js/script.js"></script>
+
+<!-- slick slider  -->
+<!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+</head>
+<body>
+    
+
+<!-- flipkart navbar  -->
+<nav>
+    <div class="container">
+ <div class="navitem">
+     <div class="logo">
+         <a href="">
+         <img src="images/flipkart-logo.png" alt="">
+         </a>
+         <a href="£">
+         Explore  <span>Plus<i class="fa fa-star-o" aria-hidden="true"></i></span>
+         </a>
+     </div>
+     <div class="search">
+         <form action="">
+       
+             <input type="search" placeholder="Search for products, brands and more">
+             
+         
+         </form>
+        </div>
+         <div class="search-icon">
+             <button><i class="fa fa-search" aria-hidden="true"></i></button>
+         </div>
+         <div class="login">
+             <button>Login</button>
+         </div>
+         <div class="seller">
+             <a href="#">Become a Seller</a>
+         </div>
+    <div class="more">
+        <a href="#">More <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+    </div>
+    <div class="cart">
+        <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</a>
+    </div>
+ </div>
+ </div>
+
+
+</nav>
+<div class="">
+    <div class="container-flued">
+     <div class="category">
+         <div class="cat">
+             <img src="images/cat/mobile.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/fashion.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/electronic.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/home.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/travel.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/appli.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/furniture.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/toys.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+         <div class="cat">
+             <img src="images/cat/grocery.webp" alt="">
+             <a href="#">Mobile</a>
+         </div>
+     </div>
+    </div>
+</div>
+
+<section class="container-flued">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/slider/1.webp" alt="First slide" width="100%" height="100%">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/slider/2.webp" alt="Second slide" width="100%" height="100%">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/slider/3.webp" alt="Third slide" width="100%" height="100%">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/slider/4.webp" alt="Third slide" width="100%" height="100%">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <i class="fa fa-angle-left" aria-hidden="true"></i>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <i class="fa fa-angle-right" aria-hidden="true"></i>
+  </a>
+</div>
+</section>
+
+
+<!-- after slider section  -->
+<section class="after-slide">
+    <div class="container-flued">
+        <div class="row">
+            <div class="col-lg-12 ">
+                <img src="images/after-slider.jpg"  alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- top offer section  -->
+<section class="offer">
+    <div class="container-flued">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-2">
+                        <div class="top">
+                            <div class="text-center">
+                              <p>  Top offer</p>
+                                <button>View All</button>
+                            </div>
+                            <div>
+                                <img src="images/left.webp" alt="">
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-8">
+                    <div class="row resources ">
+  <div class="container" id="resource-slider">
+    <button class='arrow prev'></button>
+    <button class='arrow next'></button>
+    <div class="resource-slider-frame">
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+          <div class="resource text-center">
+          <img src="images/product-slide/1.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+            
+          </div>
+        </div>
+      </div>
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+          <div class="resource">
+          <img src="images/product-slide/2.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/3.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/4.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/5.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/6.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/7.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/6.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/7.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/6.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+          <img src="images/product-slide/7.webp" alt="">
+          <br>
+          <span>pen box </span><br>
+          <a href="£">Up to 70% off</a>
+          <h5>more</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="boat">
+                            <img src="images/boar.webp" alt="">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- shop now card section -->
+
+<section class="cards">
+<div class="container-flued">
+    <div class="row">
+        <div class="col-lg-4">
+  <div class="card1">
+<img src="images/shop/1.webp" alt="">
+  </div>
+</div>
+<div class="col-lg-4">
+  <div class="card1">
+<img src="images/shop/2.webp" alt="">
+  </div>
+</div>
+<div class="col-lg-4">
+  <div class="card1">
+<img src="images/shop/3.webp" alt="">
+  </div>
+</div>
+<div class="col-lg-4">
+  <div class="card1">
+<img src="images/shop/4.webp" alt="">
+  </div>
+</div>
+<div class="col-lg-4">
+  <div class="card1">
+<img src="images/shop/5.webp" alt="">
+  </div>
+</div>
+<div class="col-lg-4">
+  <div class="card1">
+<img src="images/shop/6.webp" alt="">
+  </div>
+</div>
+ 
+
+
+    </div>
+    </div>
+</div>
+</section>
+<!-- deal section  -->
+<section class="deals">
+
+<div class="container-flued">
+    <div class="row">
+        <div class="col-lg-2">
+<div class="deal">
+    <div class="">
+    <h5>Top Deals on Feshion </h5>
+    <button>View All </button>
+    </div>
+    <div><img src="images/left-deal.webp" alt=""></div>
+</div>
+        </div>
+        <div class="col-lg-10">
+      
+<div id="myCarousel" class="carousel slide w-100" data-ride="carousel">
+    <div class="carousel-inner w-100" role="listbox">
+    <div class="row">
+
+        <div class="carousel-item active">
+
+            <div class="col-lg-3 col-md-4">
+                <!-- <img class="img-fluid" src="http://placehold.it/350x180?text=1"> -->
+                <div class="product-box">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoyKWUiL4FU6_f_f5TAF2oGPo_4BAC33YIbA&usqp=CAU" alt="image">
+                    <div class="product-name">
+                        Facebook
+                    </div>
+                    <p>Facebook is social media firm.</p>
+                    <div class="rating-div">
+                        <div class="rating">
+                            4.7 / 5
+                        </div>
+                        💚
+                    </div>
+                    <div class="knowmore">
+                        KNOW MORE
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="col-lg-3 col-md-4">
+                <!-- <img class="img-fluid" src="http://placehold.it/350x180?text=2"> -->
+                <div class="product-box">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoyKWUiL4FU6_f_f5TAF2oGPo_4BAC33YIbA&usqp=CAU" alt="image">
+                    <div class="product-name">
+                        Facebook
+                    </div>
+                    <p>Facebook is social media firm.</p>
+                    <div class="rating-div">
+                        <div class="rating">
+                            4.7 / 5
+                        </div>
+                        💚
+                    </div>
+                    <div class="knowmore">
+                        KNOW MORE
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="col-lg-3 col-md-4">
+                <!-- <img class="img-fluid" src="http://placehold.it/350x180?text=3"> -->
+                <div class="product-box">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoyKWUiL4FU6_f_f5TAF2oGPo_4BAC33YIbA&usqp=CAU" alt="image">
+                    <div class="product-name">
+                        Facebook
+                    </div>
+                    <p>Facebook is social media firm.</p>
+                    <div class="rating-div">
+                        <div class="rating">
+                            4.7 / 5
+                        </div>
+                        💚
+                    </div>
+                    <div class="knowmore">
+                        KNOW MORE
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="col-lg-3 col-md-4">
+                <!-- <img class="img-fluid" src="http://placehold.it/350x180?text=4"> -->
+                <div class="product-box">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoyKWUiL4FU6_f_f5TAF2oGPo_4BAC33YIbA&usqp=CAU" alt="image">
+                    <div class="product-name">
+                        Facebook
+                    </div>
+                    <p>Facebook is social media firm.</p>
+                    <div class="rating-div">
+                        <div class="rating">
+                            4.7 / 5
+                        </div>
+                        💚
+                    </div>
+                    <div class="knowmore">
+                        KNOW MORE
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="col-lg-3 col-md-4">
+                <!-- <img class="img-fluid" src="http://placehold.it/350x180?text=5"> -->
+                <div class="product-box">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoyKWUiL4FU6_f_f5TAF2oGPo_4BAC33YIbA&usqp=CAU" alt="image">
+                    <div class="product-name">
+                        Facebook
+                    </div>
+                    <p>Facebook is social media firm.</p>
+                    <div class="rating-div">
+                        <div class="rating">
+                            4.7 / 5
+                        </div>
+                        💚
+                    </div>
+                    <div class="knowmore">
+                        KNOW MORE
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="col-lg-3 col-md-4">
+                <!-- <img class="img-fluid" src="http://placehold.it/350x180?text=6"> -->
+                <div class="product-box">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoyKWUiL4FU6_f_f5TAF2oGPo_4BAC33YIbA&usqp=CAU" alt="image">
+                    <div class="product-name">
+                        Facebook
+                    </div>
+                    <p>Facebook is social media firm.</p>
+                    <div class="rating-div">
+                        <div class="rating">
+                            4.7 / 5
+                        </div>
+                        💚
+                    </div>
+                    <div class="knowmore">
+                        KNOW MORE
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev bg-dark w-auto" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next bg-dark w-auto" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+        </div>
+    </div>
+</div>
+
+</section>
+
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js">    </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
